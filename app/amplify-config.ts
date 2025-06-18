939938256498f6c26287b16fdb6b94ce0dd3fe32
izumi <klaviersonate14@gmail.com>
2025-06-18 12:41:50 +0900

    Add Amplify configuration for deployment

---
+++ app/amplify-config.ts
@@ -0,0 +1,8 @@
+// amplify-config.ts
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  // Your Amplify configuration will be populated here after deployment
+  // This is a placeholder that will be updated with actual values
+  ssr: true
+});
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./amplify-config";
 
 export const metadata: Metadata = {
   title: "植物育成ゲーム",
